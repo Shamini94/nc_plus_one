227428fe2b6183b7ade905ffe653b90e8cb73c1b
Shamini94 <shaminihephzibah@gmail.com>
2026-08-12 09:48:09 +0100

    Add connection.py file

---
+++ connection.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("PGHOST"),
+        port=os.getenv("PGPORT"),
+        dbname=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD")
+    )
